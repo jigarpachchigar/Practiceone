c59229d21a74929b7235c5dc4c63e104935b53db
Jigar <Jigarpachchigar5561@gmail.com>
2020-12-02 22:54:06 +0530

    Practice One Commit

--- app/src/main/java/com/example/practiceone/MainActivity.java
+++ app/src/main/java/com/example/practiceone/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.practiceone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -33,61 +35,75 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onClick(View v) {
 
-                 monthsal = Double.valueOf(etmsal.getText().toString());
-
-                 annualsal = monthsal * 12;
-
-                 etasal.setText(String.valueOf(annualsal));
-
-                 if (annualsal <= 50000)
-                 {
-                     ettxrate.setText("0%");
-                     taxrate = annualsal * 0;
-                     ettxamt.setText(String.format("%.2f",taxrate));
-                     netamt = annualsal - taxrate;
-                     etntamt.setText(String.format("%.2f",netamt));
-                     weeksal = netamt / 26;
-                     etweek.setText(String.format("%.2f",weeksal));
-
-                 }
-
-                 else if (annualsal>=50000 && annualsal<=70000 )
-                 {
-                     ettxrate.setText("17%");
-                     taxrate = annualsal * 0.17;
-                     ettxamt.setText(String.format("%.2f",taxrate));
-                     netamt = annualsal - taxrate;
-                     etntamt.setText(String.format("%.2f",netamt));
-                     weeksal = netamt / 26;
-                     etweek.setText(String.format("%.2f",weeksal));
-
-                 }
-                 else if (annualsal>=70000 && annualsal<=90000 )
-                 {
-                     ettxrate.setText("23%");
-                     taxrate = annualsal * 0.23;
-                     ettxamt.setText(String.format("%.2f",taxrate));
-                     netamt = annualsal - taxrate;
-                     etntamt.setText(String.format("%.2f",netamt));
-                     weeksal = netamt / 26;
-                     etweek.setText(String.format("%.2f",weeksal));
-                 }
-                 else
-                 {
-                     ettxrate.setText("27%");
-                     taxrate = annualsal * 0.27;
-                     ettxamt.setText(String.format("%.2f",taxrate));
-                     netamt = annualsal - taxrate;
-                     etntamt.setText(String.format("%.2f",netamt));
-                     weeksal = netamt / 26;
-                     etweek.setText(String.format("%.2f",weeksal));
-
-                 }
-
+                if (TextUtils.isEmpty(etmsal.getText().toString())) {
+                    Toast.makeText(MainActivity.this, "Please Enter Monthly Salary",
+                    Toast.LENGTH_LONG).show();
+                }
+                else {
+                    calculate();
+                }
             }
+
         });
 
     }
+
+    private void calculate() {
+
+
+            monthsal = Double.valueOf(etmsal.getText().toString());
+
+            annualsal = monthsal * 12;
+
+            etasal.setText(String.valueOf(annualsal));
+
+            if (annualsal <= 50000)
+            {
+                ettxrate.setText("0%");
+                taxrate = annualsal * 0;
+                ettxamt.setText(String.format("%.2f",taxrate));
+                netamt = annualsal - taxrate;
+                etntamt.setText(String.format("%.2f",netamt));
+                weeksal = netamt / 26;
+                etweek.setText(String.format("%.2f",weeksal));
+
+            }
+
+            else if (annualsal>=50000 && annualsal<=70000 )
+            {
+                ettxrate.setText("17%");
+                taxrate = annualsal * 0.17;
+                ettxamt.setText(String.format("%.2f",taxrate));
+                netamt = annualsal - taxrate;
+                etntamt.setText(String.format("%.2f",netamt));
+                weeksal = netamt / 26;
+                etweek.setText(String.format("%.2f",weeksal));
+
+            }
+            else if (annualsal>=70000 && annualsal<=90000 )
+            {
+                ettxrate.setText("23%");
+                taxrate = annualsal * 0.23;
+                ettxamt.setText(String.format("%.2f",taxrate));
+                netamt = annualsal - taxrate;
+                etntamt.setText(String.format("%.2f",netamt));
+                weeksal = netamt / 26;
+                etweek.setText(String.format("%.2f",weeksal));
+            }
+            else
+            {
+                ettxrate.setText("27%");
+                taxrate = annualsal * 0.27;
+                ettxamt.setText(String.format("%.2f",taxrate));
+                netamt = annualsal - taxrate;
+                etntamt.setText(String.format("%.2f",netamt));
+                weeksal = netamt / 26;
+                etweek.setText(String.format("%.2f",weeksal));
+
+            }
+
+        }
+
 
     @Override
     public void onClick(View v) {
